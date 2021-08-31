2383b34a49d2f612a2a729ad06e99fedd6c0590f
thetravisweber <tbroweber@gmail.com>
2021-08-31 10:46:28 -0700

    refactor: made legible, removed escess code

--- content.js
+++ content.js
@@ -1,24 +1,19 @@
-/* 
-  Inject Functions.js
-*/
-let s = document.createElement('script');
-s.src = chrome.runtime.getURL('functions.js');
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
-
-/* 
-  Remove Default Form Submission Behavior
+/*
+  Needed Dom Elements
 */
 let problemForm = document.getElementById("problemMainForm");
 let submitButton = document.getElementById("submitAnswers_id");
 
+/* 
+  Remove Default Form Submission Behavior
+*/
 let defaultAction = problemForm.getAttribute("action");
 problemForm.removeAttribute("action");
 problemForm.removeAttribute("onsubmit");
 
-
+/*
+  Add in Wanter Event Handlers
+*/
 document.addEventListener('keydown', function(event) {
   if (event.key == 'Enter') {
     event.preventDefault();
