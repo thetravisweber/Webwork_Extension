6490ec2d65e508d8e4726354c47ee896466248e2
thetravisweber <tbroweber@gmail.com>
2021-08-31 16:40:09 -0700

    added shortcut for next problem

--- content.js
+++ content.js
@@ -3,6 +3,8 @@
 */
 let problemForm = document.getElementById("problemMainForm");
 let submitButton = document.getElementById("submitAnswers_id");
+let aTags = [...document.getElementsByTagName("a")];
+let nextProblemButton = searchInnerText(aTags, "Next Problem");
 
 /* 
   Remove Default Form Submission Behavior
@@ -12,11 +14,22 @@ problemForm.removeAttribute("action");
 problemForm.removeAttribute("onsubmit");
 
 /*
-  Add in Wanter Event Handlers
+  Add in Wanted Event Handlers
 */
 document.addEventListener('keydown', function(event) {
   if (event.key == 'Enter') {
     event.preventDefault();
     submitButton.click();
   }
+  if (event.shiftKey) {
+    if (event.key == "N") {
+      nextProblemButton.click();
+    }
+  }
 });
+
+function searchInnerText(elements, searchText) {
+  return elements.find((el) => {
+    return el.innerHTML == searchText;
+  });
+}
