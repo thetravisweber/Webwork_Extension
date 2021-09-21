727523c66ab52e5ae66aefad841e86991662b045
thetravisweber <tbroweber@gmail.com>
2021-09-20 21:01:12 -0700

    nothing to see here

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+let specialWordsPresent = 0;
+
 [...document.getElementsByTagName("input")].map(el => {
   if (el.type === "hidden") return;
   el.addEventListener("input", function() {
@@ -5,7 +7,32 @@
   });
 });
 
-function typingAnswer() {
+function typingAnswer(el) {
   enterSendsToNextPage = false;
-  updatePreviews();
+  // updatePreviews();
+  attachEasterEggs(el.value);
+}
+
+function attachEasterEggs(answer) {
+  if (answer=="hi travis") {
+    alert("heyo 🤙");
+  } else if (containsNewlyTypedSpecialWord(answer)) {
+    alert("Watch your profanity");
+  }
+}
+
+function containsNewlyTypedSpecialWord(str) {
+  let wordCount = countSpecialWords(str);
+  if (wordCount == specialWordsPresent) return false;
+  holder = specialWordsPresent;
+  specialWordsPresent = wordCount;
+  return wordCount > holder;
+}
+
+function countSpecialWords(str) {
+  let specialWords = ['kcuf', 'tihs', 'nmad', 'hctib'];
+  specialWords = specialWords.map(word => {return word.split('').reverse().join('');});
+  let wordCount = 0;
+  specialWords.forEach((word) => {wordCount+=str.includes(word)});
+  return wordCount;
 }
