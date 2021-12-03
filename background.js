faf98d01cabc4dcbd21c3abd60d227534a7c7bc0
thetravisweber <tbroweber@gmail.com>
2021-12-03 13:43:59 -0700

    removed unneccessary logs

--- background.js
+++ background.js
@@ -1,14 +1,3 @@
-console.log("am running");
-
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("yeee");
-  console.log(tab);
   chrome.tabs.sendMessage(tab.id, 'open_random_problem');
 });
-
-
-
-// chrome.webNavigation.onCompleted.addListener(function() {
-//   alert("This is my favorite website!");
-// });
